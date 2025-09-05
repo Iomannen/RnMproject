67ae17de96512020630b6c73e42ba2717e0bb54f
Iomannen <maxymdavidov@gmail.com>
2025-09-05 11:26:30 +0300

    visual updateч

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip', // можно 'brotliCompress'
+      ext: '.gz', // расширение
+      threshold: 10240, // сжимать файлы >10kb
+    }),
+    compression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 10240,
+    }),
+  ],
+});
